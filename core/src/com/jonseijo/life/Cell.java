dca3abe8d366b69a4d6d52e7caff609f983dacfa
jonseijo <jon.seijo@gmail.com>
2016-12-04 19:02:32 -0300

    Celula conoce su tamaño

--- core/src/com/jonseijo/life/Cell.java
+++ core/src/com/jonseijo/life/Cell.java
@@ -5,6 +5,13 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Cell {
 
+    /*
+     No es correcto que este harcodeado.
+     La idea seria alrevez, que la textura que cargo dependa del tamaño que seteo.
+     Esto hay que arreglarlo, pero para esta etapa no vale invertir tanto tiempo.
+    */
+    public static final int size = 32;
+
     private boolean alive;
     private Texture texture;
 
@@ -16,8 +23,8 @@ public class Cell {
         this.setAlive(isAlive);
     }
 
-    public void render(SpriteBatch batch) {
-        // Todavia no hace nada, tengo que ver quien y como controla la posicion
+    public void render(SpriteBatch batch, int x, int y) {
+        batch.draw(this.texture, x, y);
     }
 
     public boolean isAlive() {
@@ -28,5 +35,6 @@ public class Cell {
         this.alive = alive;
         this.texture = alive ? AssetLoader.cell_alive : AssetLoader.cell_dead;
     }
+
 
 }
