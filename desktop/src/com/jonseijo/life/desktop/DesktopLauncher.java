41d954f2d02a703cfa6df9138ff03df73da29cc2
jonseijo <jon.seijo@gmail.com>
2016-12-04 18:00:57 -0300

    Initial commit

---
+++ desktop/src/com/jonseijo/life/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jonseijo.life.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jonseijo.life.GameLife;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameLife(), config);
+	}
+}
