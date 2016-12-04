0e80976ed0debb7e4a7cd7183a441e9a9cbbd101
jonseijo <jon.seijo@gmail.com>
2016-12-04 19:28:10 -0300

    Creo clase encargada de aplicar las reglas

---
+++ core/src/com/jonseijo/life/Jon.java
@@ -0,0 +1,22 @@
+package com.jonseijo.life;
+
+public class Jon {
+
+    public Jon() {
+
+    }
+
+    /*
+    Este metodo es el encargado de aplicar las reglas de la vida,
+    modificando el tablero para llevarlo a un nuevo estado.
+
+    1) Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+    2) Any live cell with two or three live neighbours lives on to the next generation.
+    3) Any live cell with more than three live neighbours dies, as if by over-population.
+    4) Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+    */
+    public void simular(Tablero tablero) {
+
+    }
+}
