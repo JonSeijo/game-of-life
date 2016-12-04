5483f20b996b3fc923ce920a6023b3224a8d78bb
jonseijo <jon.seijo@gmail.com>
2016-12-04 18:46:47 -0300

    Celula conoce su textura correspondiente

--- core/src/com/jonseijo/life/Cell.java
+++ core/src/com/jonseijo/life/Cell.java
@@ -1,19 +1,32 @@
 package com.jonseijo.life;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 public class Cell {
 
-    boolean alive;
+    private boolean alive;
+    private Texture texture;
 
     public Cell() {
         this(false);
     }
 
     public Cell(boolean isAlive) {
-        this.alive = isAlive;
+        this.setAlive(isAlive);
+    }
+
+    public void render(SpriteBatch batch) {
+        // Todavia no hace nada, tengo que ver quien y como controla la posicion
     }
 
     public boolean isAlive() {
         return this.alive;
+    }
+
+    public void setAlive(boolean alive) {
+        this.alive = alive;
+        this.texture = alive ? AssetLoader.cell_alive : AssetLoader.cell_dead;
     }
 
 }
