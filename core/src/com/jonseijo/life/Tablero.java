ff6a62befd7b0050966b5803cb84ebc04725ca8a
jonseijo <jon.seijo@gmail.com>
2016-12-04 20:30:08 -0300

    Arregla bug en cuenta de vecinos vivos

--- core/src/com/jonseijo/life/Tablero.java
+++ core/src/com/jonseijo/life/Tablero.java
@@ -70,7 +70,7 @@ public class Tablero {
         int vivos = 0;
         for (int i = x - 1; i <= x + 1; i++) {
             for (int j = y - 1; j <= y + 1; j++) {
-                if (i != x && j != y) {
+                if (!(i == x && j == y)) {
                     vivos += this.getN(i, j);
                 }
             }
